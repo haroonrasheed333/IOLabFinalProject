efb3b31d86c5c49a3de94266177579558eca3c35
Prabha Matta <prabha@WhiteBook.local>
2012-11-27 23:21:09 -0800

    Added twitter data

--- js/main.js
+++ js/main.js
@@ -1,19 +1,8 @@
-  
+  $(function() {
 
-
-
-$(function() {
-    $('#showResults').click(function() {
-        codeAddress();
-        getDescription();
-        showResults();
-    });
-});
-
-
-
-    var geocoder;
-    var map;
+  var queryTerm;
+  var geocoder;
+  var map;
 
     function codeAddress() {
       console.log("Entered function Code address")
@@ -57,3 +46,38 @@ $(function() {
         $("#wikidiv1").append('<p id="description">' + feed + '</p>');
       });
     }
+
+    // Function to get twitter feed
+    function getTwitter(){
+      $('#twitter').empty();
+      var twitterQuery = queryTerm;
+      twitterQuery = twitterQuery.replace(/\s+/g,"%20");
+      var tweetURL =  'http://search.twitter.com/search.json?q=%23' + twitterQuery + '&callback=?&include_entities=true&rpp=100';
+      
+      $.getJSON(tweetURL, function(json){
+        tweets = json.results;
+        if(tweets.length == 0)
+        {
+            console.log("No tweets");
+        }
+        else 
+        {  
+            for(var i = 0; i < 10; i++) 
+            {
+             
+
+              $('#twitter').append('<li class="term">'+ tweets[i].text +'</li>')
+            }  
+                 
+          };
+        });
+      };
+
+     $('#showResults').click(function() {
+      queryTerm = $('#myinput').val();
+        codeAddress();
+        getDescription();
+        showResults();
+        getTwitter();
+    });
+});
