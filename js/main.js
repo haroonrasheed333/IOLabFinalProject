2b090dac3edbf2fbb661504a7702397cffb8a932
Bharath <bharathkumarguna@gmail.com>
2012-11-29 02:04:12 -0800

    youtube merge

--- js/main.js
+++ js/main.js
@@ -135,6 +135,100 @@
               });
       }
 
+    function getVid(input) {
+        var youtubeURL =  'http://gdata.youtube.com/feeds/api/videos?q=' + queryTerm + '&alt=json-in-script&callback=?&max-results=10';
+
+        $.getJSON(youtubeURL, function(json){
+        var feed = json.feed;
+      var entries = feed.entry || [];
+      var html = ['<ul class="videos">'];
+      for (var i = 0; i < entries.length; i++) {
+        var entry = entries[i];
+        var thumbnailUrl = entries[i].media$group.media$thumbnail[0].url;
+        var playerUrl = entries[i].media$group.media$content[0].url;
+        html.push('<li>', '<a class="fancybox-media" href="', playerUrl, '"><img src="', thumbnailUrl, '" width="130" height="97"/></a>', '</li>');
+      }
+      html.push('</ul><br style="clear: left;"/>');
+      document.getElementById('newsdivbot1').innerHTML = html.join('');
+        });
+    }
+
+    function getNYTimes(input) {
+      query1 = document.getElementById("myinput").value.replace(/ /g,"+").toLowerCase();
+        var articleURL = 'http://api.nytimes.com/svc/search/v1/article?format=json&query=' + query1 + '&api-key=579c0d468cffcc963fc547b5e45cb65c:14:66734303';
+      var phpproxy = 'http://people.ischool.berkeley.edu/~haroon/IOLab/samp.php?callback=?'
+      $.getJSON(phpproxy, {"url": articleURL}, function(data){
+        console.log(data.results);
+        var h = document.createElement('h1');
+          $(h).append("NYTIMES");
+          $('#nytimes').append(h);
+        var html = ['<ul class="news">'];
+        for (var i = 0; i < 5; i++)  {
+            var url = data.results[i].url;        
+            var phpproxy2 = 'http://people.ischool.berkeley.edu/~haroon/IOLab/samp2.php?callback=?'
+            var newsURL = 'http://api.nytimes.com/svc/news/v3/content.json?&url='+url+'&api-key=6b8d475bf3699ab7851fc40722dc1235:7:66734303';
+            $.getJSON(phpproxy2, {"url": newsURL}, function(data2){
+              console.log(data2);
+              result = data2.results[0];
+              title = result.title;
+              abstract = result.abstract;
+              urll = result.url;
+              console.log(title);
+              //html.push('<li>Title: ', title , ' Abstract: ', abstract, ' url=', urll , '</li>');
+              $('<li></li>').html('Title: ' + title + ' Abstract: ' + abstract + ' url=' + urll)
+                .appendTo('.news');
+            });
+          }
+          html.push('</ul><br/>');
+          document.getElementById('news2').innerHTML = html.join('');
+
+      });
+    }
+
+    function bingNewsSearch(query, service)
+    {
+      $.getJSON('http://people.ischool.berkeley.edu/~haroon/IOLab/bing_proxy.php?callback=?', {q: query, sop: service}, function(data)
+      {
+        if (data.d !== undefined)
+        {
+          var items = data.d.results;
+
+          var h = document.createElement('h1');
+        var html = ['<ul class="news">'];
+          for (var i = 0 ; i < items.length; i++)
+          {
+            var item = items[i];
+            var p = document.createElement('p');
+            var a = document.createElement('a');
+            a.href = item.Url;
+            $(a).append(item.Title);
+            $(p).append(item.Description);
+            //$('#newsdivtop2').append(a);
+            $('<li></li>').html(a)
+                .appendTo('#newsdivtop2');
+          }
+          html.push('</ul><br/>');
+        }
+      });
+    }
+
+
+    $('.fancybox-media')
+      .attr('rel', 'media-gallery')
+      .fancybox({
+        openEffect : 'none',
+        closeEffect : 'none',
+        prevEffect : 'none',
+        nextEffect : 'none',
+        arrows : false,
+        helpers : {
+          media : {},
+          buttons : {}
+        }
+      });
+
+
+
      $('#showResults').click(function() {
       queryTerm = $('#myinput').val();
         codeAddress();
@@ -142,6 +236,9 @@
         getTwitter();
         getGooglenews();
         getFlickr();
+        getVid();
+        getNYTimes();
+        bingNewsSearch(queryTerm, "News");
         showResults();
     });
 });
