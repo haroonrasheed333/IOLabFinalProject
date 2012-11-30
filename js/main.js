221b3d60ea1c25129a3eb4b6feab3cb135bc6d3d
Prabha Matta <prabha@WhiteBook.local>
2012-11-30 01:43:12 -0800

    Bing News done

--- js/main.js
+++ js/main.js
@@ -112,6 +112,87 @@
             };
           });
       }
+  function bingNewsSearch(query, service)
+    {
+      $.getJSON('http://people.ischool.berkeley.edu/~haroon/IOLab/bing_proxy.php?callback=?', {q: query, sop: service}, function(data)
+      {
+        if (data.d !== undefined)
+        {
+          var items = data.d.results;
+          for (var i = 0 ; i < items.length; i++)
+          {
+            var item = items[i];
+            $('#bing').append('<li class="term"><a href="'+item.Url+'" title="'+item.Description+'">"'+ item.Title+'</li>');
+          
+          }
+        }
+      });
+    }
+      function getNYTimes(){
+
+        $('#googlenews').empty();
+        var Query = queryTerm;
+        Query = Query.replace(/\s+/g,"+");
+        var URL = 'http://api.nytimes.com/svc/search/v1/article?format=json&query=' + Query + '&api-key=579c0d468cffcc963fc547b5e45cb65c:14:66734303&callback=?';
+        $.getJSON(URL, function(json){
+          Results = json.results;
+          console.log(googleResults.length)
+          if(Results.length == 0)
+          {
+              console.log("No News from NY Times");
+          }
+          else 
+          {  
+              for(var i = 0; i < Results.length; i++) 
+              {
+                 var newsContent = Results[i].content
+                 newsContent = newsContent.replace(/<b>/g,"");
+                  newsContent = newsContent.replace(/<\/b>/g,"");     
+     
+                $('#NYTimes').append('<li class="term"><a href="'+Results[i].signedRedirectUrl+'" title="'+newsContent+'">"'+ Results[i].titleNoFormatting+'</li>');
+
+              }  
+                   
+            };
+          });
+      }
+
+
+       function getNYTimes2(input) {
+      query1 = document.getElementById("myinput").value.replace(/ /g,"+").toLowerCase();
+        var articleURL = 'http://api.nytimes.com/svc/search/v1/article?format=json&query=' + query1 + '&api-key=579c0d468cffcc963fc547b5e45cb65c:14:66734303&callback=?';
+      // var phpproxy = 'http://people.ischool.berkeley.edu/~haroon/IOLab/samp.php?callback=?'
+      // $.getJSON(phpproxy, {"url": articleURL}, function(data){
+      $.getJSON(articleURL, function(data){
+        console.log(data.results);
+        var h = document.createElement('h1');
+          $(h).append("NYTIMES");
+          $('#nytimes').append(h);
+        var html = ['<ul class="news">'];
+        for (var i = 0; i < 5; i++)  {
+            var url = data.results[i].url;        
+            var phpproxy2 = 'http://people.ischool.berkeley.edu/~haroon/IOLab/samp2.php?callback=?'
+            var newsURL = 'http://api.nytimes.com/svc/news/v3/content.json?&url='+url+'&api-key=6b8d475bf3699ab7851fc40722dc1235:7:66734303';
+            $.getJSON(phpproxy2, {"url": newsURL}, function(data2){
+              console.log(data2);
+              result = data2.results[0];
+              title = result.title;
+              abstract = result.abstract;
+              urll = result.url;
+              console.log(title);
+              //html.push('<li>Title: ', title , ' Abstract: ', abstract, ' url=', urll , '</li>');
+              $('<li></li>').html('Title: ' + title + ' Abstract: ' + abstract + ' url=' + urll)
+                .appendTo('.news');
+            });
+          }
+          html.push('</ul><br/>');
+          document.getElementById('news2').innerHTML = html.join('');
+
+      });
+    }
+
+ 
+
 
       function getFlickr(){
 
@@ -153,65 +234,7 @@
         });
     }
 
-    function getNYTimes(input) {
-      query1 = document.getElementById("myinput").value.replace(/ /g,"+").toLowerCase();
-        var articleURL = 'http://api.nytimes.com/svc/search/v1/article?format=json&query=' + query1 + '&api-key=579c0d468cffcc963fc547b5e45cb65c:14:66734303';
-      var phpproxy = 'http://people.ischool.berkeley.edu/~haroon/IOLab/samp.php?callback=?'
-      $.getJSON(phpproxy, {"url": articleURL}, function(data){
-        console.log(data.results);
-        var h = document.createElement('h1');
-          $(h).append("NYTIMES");
-          $('#nytimes').append(h);
-        var html = ['<ul class="news">'];
-        for (var i = 0; i < 5; i++)  {
-            var url = data.results[i].url;        
-            var phpproxy2 = 'http://people.ischool.berkeley.edu/~haroon/IOLab/samp2.php?callback=?'
-            var newsURL = 'http://api.nytimes.com/svc/news/v3/content.json?&url='+url+'&api-key=6b8d475bf3699ab7851fc40722dc1235:7:66734303';
-            $.getJSON(phpproxy2, {"url": newsURL}, function(data2){
-              console.log(data2);
-              result = data2.results[0];
-              title = result.title;
-              abstract = result.abstract;
-              urll = result.url;
-              console.log(title);
-              //html.push('<li>Title: ', title , ' Abstract: ', abstract, ' url=', urll , '</li>');
-              $('<li></li>').html('Title: ' + title + ' Abstract: ' + abstract + ' url=' + urll)
-                .appendTo('.news');
-            });
-          }
-          html.push('</ul><br/>');
-          document.getElementById('news2').innerHTML = html.join('');
-
-      });
-    }
-
-    function bingNewsSearch(query, service)
-    {
-      $.getJSON('http://people.ischool.berkeley.edu/~haroon/IOLab/bing_proxy.php?callback=?', {q: query, sop: service}, function(data)
-      {
-        if (data.d !== undefined)
-        {
-          var items = data.d.results;
-
-          var h = document.createElement('h1');
-        var html = ['<ul class="news">'];
-          for (var i = 0 ; i < items.length; i++)
-          {
-            var item = items[i];
-            var p = document.createElement('p');
-            var a = document.createElement('a');
-            a.href = item.Url;
-            $(a).append(item.Title);
-            $(p).append(item.Description);
-            //$('#newsdivtop2').append(a);
-            $('<li></li>').html(a)
-                .appendTo('#newsdivtop2');
-          }
-          html.push('</ul><br/>');
-        }
-      });
-    }
-
+   
 
     $('.fancybox-media')
       .attr('rel', 'media-gallery')
